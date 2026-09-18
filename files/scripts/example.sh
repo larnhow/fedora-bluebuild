95a6d1502e216c5206a6ae302dea4182699313ec
larnhow <larnhow@gmail.com>
2026-09-18 18:15:23 +0200

    chrome

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -8,3 +8,7 @@ set -oue pipefail
 # Your code goes here.
 echo 'This is an example shell script'
 echo 'Scripts here will run during build if specified in recipe.yml'
+
+dnf -y install fedora-workstation-repositories
+dnf config-manager setopt google-chrome.enabled=1
+dnf -y install google-chrome-stable
